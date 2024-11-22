6b5626b4f578d1659735c385c6d88ea7bd866bd0
Qiang <qiangyu@gmail.com>
2024-11-21 23:04:36 -0800

    haha. The blob looks working

--- c/src/bprm_committed_creds/probe.bpf.c
+++ c/src/bprm_committed_creds/probe.bpf.c
@@ -10,6 +10,7 @@
 #include <bpf_helpers.h>
 #include <bpf_tracing.h>
 
+#include "common/blob.h"
 #include "common/bprm.h"
 #include "common/task.h"
 #include "common/vmlinux.h"
@@ -18,9 +19,20 @@ char _license[] SEC("license") = "GPL";
 
 SEC("lsm/bprm_committed_creds")
 int BPF_PROG(bprm_committed_creds, const struct linux_binprm *bprm) {
-  struct task_struct *curr = bpf_get_current_task_btf();
-  lw_task_struct lw_task;
-  parse_task(curr, &lw_task);
-  parse_binprm(bprm);
+  lw_blob * blob = reserve_blob();
+  if (!blob) {
+    return 0;
+  }
+
+  bpf_printk("blob1 -> %s", bprm->filename);
+  long l1 = bpf_core_read_str(blob->data, 100, bprm->filename);
+  bpf_printk("blob1 -> %s %d", blob->data, l1);
+  blob = next_blob(blob);
+  if (!blob) {
+    return 0;
+  }
+  l1 = bpf_core_read_str(blob->data, 100, bprm->filename + 100);
+  bpf_printk("blob2 -> %s %d", blob->data, l1);
+  submit_blob(blob);
   return 0;
 }
--- c/src/common/blob.h
+++ c/src/common/blob.h
@@ -25,4 +25,62 @@ typedef struct _lw_blob_ {
   uint8_t data[BLOB_SIZE - 24];
 } lw_blob;
 
+struct {
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __type(key, uint32_t);
+  __type(value, uint64_t);
+  __uint(max_entries, 1);
+} _blob_index_ SEC(".maps");
+
+struct {
+  __uint(type, BPF_MAP_TYPE_RINGBUF);
+  __uint(max_entries, BLOB_MAP_ENTRIES);
+} _blob_ringbuf_ SEC(".maps");
+
+static inline lw_blob* reserve_blob() {
+  uint32_t zero = 0;
+  uint64_t* blob_id = bpf_map_lookup_elem(&_blob_index_, &zero);
+  if (!blob_id) {
+    // Cannot happen.
+    return 0;
+  }
+
+  lw_blob *blob = bpf_ringbuf_reserve(&_blob_ringbuf_, sizeof(lw_blob), 0);
+  if (!blob) {
+    return 0;
+  }
+
+  blob-> version = 0x01;
+  blob->data_size = 0;
+  blob->blob_id = *blob_id;
+  blob->blob_next = 0;
+
+  *blob_id = *blob_id + 1;
+  bpf_map_update_elem(&_blob_index_, &zero, blob_id, BPF_ANY);
+
+  return blob;
+}
+
+static inline void submit_blob(lw_blob *blob) {
+  bpf_ringbuf_submit(blob, 0);
+}
+
+static inline void discard_blob(lw_blob *blob) {
+  bpf_ringbuf_discard(blob, 0);
+}
+
+static inline lw_blob* next_blob(lw_blob *blob) {
+  if (!blob) {
+    return 0;
+  }
+
+  lw_blob *next_blob = reserve_blob();
+  if (next_blob) {
+    blob->blob_next = next_blob->blob_id;
+  }
+
+  submit_blob(blob);
+  return next_blob;
+}
+
 #endif
