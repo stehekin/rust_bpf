274b0bb6dd9aead4528cd6fed5dfd09f6f454a95
Qiang <qiangyu@gmail.com>
2024-11-22 22:12:17 -0800

    enhance blob

--- c/src/bprm_committed_creds/probe.bpf.c
+++ c/src/bprm_committed_creds/probe.bpf.c
@@ -12,7 +12,7 @@
 
 #include "common/blob.h"
 #include "common/bprm.h"
-#include "common/task.h"
+#include "common/signal.h"
 #include "common/vmlinux.h"
 
 char _license[] SEC("license") = "GPL";
@@ -24,15 +24,12 @@ int BPF_PROG(bprm_committed_creds, const struct linux_binprm *bprm) {
     return 0;
   }
 
-  bpf_printk("blob1 -> %s", bprm->filename);
   long l1 = bpf_core_read_str(blob->data, 100, bprm->filename);
-  bpf_printk("blob1 -> %s %d", blob->data, l1);
   blob = next_blob(blob);
   if (!blob) {
     return 0;
   }
   l1 = bpf_core_read_str(blob->data, 100, bprm->filename + 100);
-  bpf_printk("blob2 -> %s %d", blob->data, l1);
   submit_blob(blob);
   return 0;
 }
--- c/src/common/blob.h
+++ c/src/common/blob.h
@@ -15,14 +15,16 @@
 
 #define BLOB_SIZE 1024
 #define BLOB_MAP_ENTRIES 1024 * BLOB_SIZE
+#define BLOB_DATA_SIZE (BLOB_SIZE - 24)
 
-typedef struct _lw_blob_ {
+typedef struct {
   uint16_t version;
   uint16_t data_size;
   uint32_t reserved;
   uint64_t blob_id;
   uint64_t blob_next;
-  uint8_t data[BLOB_SIZE - 24];
+  // Must update BLOB_DATA_SIZE if updating lw_blob.
+  uint8_t data[BLOB_DATA_SIZE];
 } lw_blob;
 
 struct {
@@ -69,6 +71,8 @@ static inline void discard_blob(lw_blob *blob) {
   bpf_ringbuf_discard(blob, 0);
 }
 
+// `next_blob` reserves a new blob and links it to `blob`.
+// `next_blob` submits the given `blob`.
 static inline lw_blob* next_blob(lw_blob *blob) {
   if (!blob) {
     return 0;
@@ -81,6 +85,66 @@ static inline lw_blob* next_blob(lw_blob *blob) {
 
   submit_blob(blob);
   return next_blob;
+}
+
+// `copy_str` copies str to blobs. This function
+// * returns 0 if it has succeeded;
+// * returns -1 if it has failed.
+//
+// `blob_id` is the first blob submitted, even if the function has failed.
+// If no blobs are submitted, `blob_id` is -1.
+//
+// The last byte of all blobs submitted is NUL.
+//
+// Maximum blobs supported by this function is 256.
+#define MAX_BLOBS 256
+static inline int32_t copy_str_to_blob(uint8_t *str, uint64_t *blob_id) {
+  int32_t rv = -1;
+  long total_copied = 0;
+
+  lw_blob * blob = reserve_blob();
+  for (uint16_t i = 0; i < MAX_BLOBS && blob; i++) {
+    if (i == 0) {
+      *blob_id = blob->blob_id;
+    }
+
+    long l1 = bpf_probe_read_kernel_str(blob->data, BLOB_DATA_SIZE, str + total_copied);
+
+    if (l1 < 0) {
+      break;
+    } else if (l1 < BLOB_DATA_SIZE) {
+      rv = 0;
+      break;
+    }
+
+    total_copied += BLOB_DATA_SIZE - 1;
+
+    uint8_t last;
+    bpf_probe_read_kernel(&last, 1, str + total_copied);
+    // No more blobs needed.
+    if (last == 0) {
+      rv = 0;
+      break;
+    }
+
+    if (i == MAX_BLOBS - 1) {
+      submit_blob(blob);
+      blob = 0;
+      break;
+    }
+
+    blob = next_blob(blob);
+  }
+
+  if (blob) {
+    if (rv == 0) {
+      submit_blob(blob);
+    } else {
+      discard_blob(blob);
+    }
+  }
+
+  return rv;
 }
 
 #endif
