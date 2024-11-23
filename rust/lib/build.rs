c2a5ece6a27434515f2f90657614aa6d66f912d1
Qiang <qiangyu@gmail.com>
2024-11-23 11:34:28 -0800

    test

--- rust/lib/build.rs
+++ rust/lib/build.rs
@@ -9,21 +9,29 @@ fn main() {
 fn build_bpf(c_bpf_dir: &str) {
     let include = format!("-I{c_bpf_dir}");
     let args = vec![
-      "-D__TARGET_ARCH_x86",
-      "-D__BPF_TRACING__",
-      "-DCORE",
-      "-I/usr/include/bpf",
-      include.as_str(),
+        "-D__TARGET_ARCH_x86",
+        "-D__BPF_TRACING__",
+        "-DCORE",
+        "-I/usr/include/bpf",
+        include.as_str(),
     ];
-    let source = format!("{0}/file_open/probe.bpf.c", c_bpf_dir);
-    let target = "src/bpf/file_open.rs";
 
-    let mut builder = SkeletonBuilder::new();
-    builder
-        .clang_args(args)
-        .source(source)
-        .build_and_generate(target)
-        .expect("cannot build bpf");
+    let bpfs = vec![
+        "file_open",
+        "bprm_committed_creds",
+    ];
+
+    for bpf in bpfs {
+        let source = format!("{0}/{1}/probe.bpf.c", c_bpf_dir, bpf);
+        let target = format!("src/bpf/{0}.rs", bpf);
+
+        let mut builder = SkeletonBuilder::new();
+        builder
+            .clang_args(args.clone())
+            .source(source)
+            .build_and_generate(target)
+            .expect("cannot build bpf");
+    }
 }
 
 fn bindgen() {
