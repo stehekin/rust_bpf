18eaf4d05f70f9ffef6298beb94d9c5b2f26a03a
Qiang <qiangyu@gmail.com>
2024-11-23 11:53:45 -0800

    fixed the test

--- rust/lib/src/tests/file_open_test.rs
+++ rust/lib/src/tests/file_open_test.rs
@@ -1,8 +1,7 @@
-use std::os::linux::fs::MetadataExt;
+use std::mem::MaybeUninit;
 use std::time::Duration;
 use anyhow::Result;
 use crate::bpf::file_open;
-use crate::bpf::file_open_util;
 use crate::bpf::file_open::ProbeSkel;
 
 use libbpf_rs::{
@@ -13,15 +12,18 @@ use crate::bpf::file_open_util::ProbeSkelExt;
 
 #[test]
 fn test_file_open() {
-  let skel = load_bpf().unwrap();
+  let mut open_object = MaybeUninit::uninit();
+  let skel = load_bpf(&mut open_object).unwrap();
   skel.monitor_file("/tmp/test").unwrap();
   std::thread::sleep(Duration::from_secs(3600));
 }
 
-fn load_bpf<'a>() -> Result<ProbeSkel<'a>> {
+fn load_bpf(open_object: &mut MaybeUninit<libbpf_rs::OpenObject>) -> Result<ProbeSkel> {
   let builder = file_open::ProbeSkelBuilder::default();
-  let mut open_skel = builder.open()?;
+
+  let mut open_skel = builder.open(open_object)?;
   let mut skel = open_skel.load()?;
   skel.attach()?;
+
   Ok(skel)
 }
