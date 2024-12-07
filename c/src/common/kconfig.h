0635d6f89df48d686c857271c471ba58f75d9fbf
Qiang <qiangyu@gmail.com>
2024-12-07 07:37:45 -0800

    detracee

--- c/src/common/kconfig.h
+++ c/src/common/kconfig.h
@@ -1,15 +1,4 @@
 #ifndef _LW_KCONFIG_H_
 #define _LW_KCONFIG_H_
 
-#include "common/int_types.h"
-
-enum kconfig_key_e {
-    ARCH_HAS_SYSCALL_WRAPPER = 1000U
-};
-
-// TODO: support get_kconfig.
-static int get_kconfig(u32 key) {
-  return 1;
-}
-
 #endif
