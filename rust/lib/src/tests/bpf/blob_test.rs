aca25c1a799ecd54fca5c97fdf121413a36e97c5
Qiang <qiangyu@gmail.com>
2024-12-10 21:16:36 -0800

    complete the test of blob merging

--- rust/lib/src/tests/bpf/blob_test.rs
+++ rust/lib/src/tests/bpf/blob_test.rs
@@ -71,3 +71,72 @@ async fn test_blob_reader_merge() {
 
     assert_eq!(merged.as_slice(), data);
 }
+
+#[tokio::test]
+async fn test_blob_reader_merge_with_missing_blobs() {
+    let (sender, receiver) = async_channel::unbounded();
+    let cpu = 0;
+    let seq = 2;
+    let data = "012345678".as_bytes();
+
+    let mut reader = BlobReceiver::new(cpu, receiver);
+
+    let r= tokio::spawn(async move {
+        let mut merged = vec![];
+        let r = merge_blobs(seq_to_blob_id(cpu, seq), &mut merged, &mut reader).await;
+        (merged, r)
+    });
+
+    sleep(time::Duration::from_secs(2));
+
+    let w = tokio::spawn(async move {
+        sender.send(fake_blob(cpu, seq, 9, Some(&data[0..1]))).await.expect("error sending blob");
+        sender.send(fake_blob(cpu, 11, 0, Some(&data[6..data.len()]))).await.expect("error sending blob");
+    });
+
+    w.await.expect("error channel writing");
+    let (merged, result) = r.await.expect("error merging blobs");
+
+    assert!(result.is_err());
+    assert_eq!(merged.as_slice(), &data[0..1]);
+}
+
+
+#[tokio::test]
+// Interleaved blocks should never happen in the real life. But it is a good test case for the `merge_blobs`.
+async fn test_blob_reader_merge_interleaved_blocks() {
+    let (sender, receiver) = async_channel::unbounded();
+    let cpu = 0;
+    let seq_1 = 2;
+    let seq_2 = 7;
+    let data = "012345678".as_bytes();
+
+    let mut reader = BlobReceiver::new(cpu, receiver);
+
+    let r= tokio::spawn(async move {
+        let mut merged_1 = vec![];
+        let mut merged_2 = vec![];
+        merge_blobs(seq_to_blob_id(cpu, seq_1), &mut merged_1, &mut reader).await.expect("error merging blobs");
+        let result_2 = merge_blobs(seq_to_blob_id(cpu, seq_2), &mut merged_2, &mut reader).await;
+        (merged_1, result_2)
+    });
+
+    sleep(time::Duration::from_secs(2));
+
+    let w = tokio::spawn(async move {
+        //  Sequences:
+        //  data_1:  2,   9, 11
+        //  data_2:     7
+        //  data_2 is be discarded by when merging blob_1 as 7 < 9.
+        sender.send(fake_blob(cpu, seq_1, 9, Some(&data[0..1]))).await.expect("error sending blob");
+        sender.send(fake_blob(cpu, seq_2, 0, Some(&data[0..1]))).await.expect("error sending blob");
+        sender.send(fake_blob(cpu, 9, 11, Some(&data[1..6]))).await.expect("error sending blob");
+        sender.send(fake_blob(cpu, 11, 0, Some(&data[6..data.len()]))).await.expect("error sending blob");
+    });
+
+    w.await.expect("error channel writing");
+    let (merged_1, result_2) = r.await.expect("error merging blobs");
+
+    assert_eq!(merged_1.as_slice(), data);
+    assert!(result_2.is_err());
+}
