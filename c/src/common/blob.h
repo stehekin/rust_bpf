8aaa72ff178074a16cc81e586c044ce8c8f260c8
qiang <qiangyu@gmail.com>
2025-01-23 16:20:01 +0000

    refactor blob.h

--- c/src/common/blob.h
+++ c/src/common/blob.h
@@ -14,18 +14,17 @@
 #include <bpf_helpers.h>
 #include <bpf_tracing.h>
 
-#define MAX_BLOBS 16
+#define MAX_BLOBS 32
 #define BLOB_LOOP_CONTINUE 0
 #define BLOB_LOOP_BREAK 1
 
 typedef struct {
     u64 data_len;
     u64 data_ptr;
-    u64 *blob_id;
-    void *src;
+    u64  blob_id;
     bool is_kernel;
-    lw_blob *blob;
-    s32 rv;
+    s32 return_value;
+    void *src;
 } blob_loop_context;
 
 static inline u64 create_blob_id(u64 v) {
@@ -34,6 +33,35 @@ static inline u64 create_blob_id(u64 v) {
   return result;
 }
 
+static inline u64 next_blob_id() {
+    u32 zero = 0;
+    u64* blob_id = bpf_map_lookup_elem(&_blob_index_, &zero);
+    if (!blob_id) {
+      // Cannot happen.
+      return 0;
+    }
+
+    // First blob_id is 1 (skipping 0);
+    *blob_id += 1;
+    bpf_map_update_elem(&_blob_index_, &zero, blob_id, BPF_ANY);
+    return create_blob_id(*blob_id);
+}
+
+static lw_blob* reserve_blob_with_id(u64 blob_id) {
+    lw_blob *blob = bpf_ringbuf_reserve(&_blob_ringbuf_, BLOB_SIZE, 0);
+    if (!blob) {
+      return 0;
+    }
+
+    blob->header.blob_size = BLOB_SIZE;
+    blob->header.effective_data_size = 0;
+    blob->header.blob_id = blob_id;
+    blob->header.blob_next = 0;
+
+    return blob;
+}
+
+// To be deprecated.
 static lw_blob* reserve_blob() {
   u32 zero = 0;
   u64* blob_id = bpf_map_lookup_elem(&_blob_index_, &zero);
@@ -60,13 +88,14 @@ static lw_blob* reserve_blob() {
 }
 
 static inline void submit_blob(lw_blob *blob) {
-  bpf_ringbuf_submit(blob, 0);
+    bpf_ringbuf_submit(blob, 0);
 }
 
 static inline void discard_blob(lw_blob *blob) {
-  bpf_ringbuf_discard(blob, 0);
+    bpf_ringbuf_discard(blob, 0);
 }
 
+// To be deprecated.
 // `next_blob` reserves a new blob and links it to `blob`. The new blob has the same size of the given `blob`.
 // `next_blob` submits the given `blob`.
 static inline lw_blob *next_blob(lw_blob *blob) {
@@ -84,12 +113,10 @@ static inline lw_blob *next_blob(lw_blob *blob) {
 }
 
 static long blob_loop_func(u32 i, blob_loop_context *ctx) {
-    if (!ctx->blob) {
-        return BLOB_LOOP_BREAK;
-    }
+    lw_blob *blob = reserve_blob_with_id(ctx->blob_id);
 
-    if (i == 0) {
-      *(ctx->blob_id) = ctx->blob->header.blob_id;
+    if (!blob) {
+        return BLOB_LOOP_BREAK;
     }
 
     u64 to_copy = ctx->data_len - ctx->data_ptr;
@@ -100,43 +127,37 @@ static long blob_loop_func(u32 i, blob_loop_context *ctx) {
     long result = 0;
 
     if (ctx->is_kernel) {
-      result = bpf_probe_read_kernel(ctx->blob->data, to_copy, ctx->src + ctx->data_ptr);
+      result = bpf_probe_read_kernel(blob->data, to_copy, ctx->src + ctx->data_ptr);
     } else {
-      result = bpf_probe_read_user(ctx->blob->data, to_copy, ctx->src + ctx->data_ptr);
+      result = bpf_probe_read_user(blob->data, to_copy, ctx->src + ctx->data_ptr);
     }
 
     if (result < 0) {
-      if (i == 0) {
-        *(ctx->blob_id) = 0;
-      }
-      return BLOB_LOOP_BREAK;
+        discard_blob(blob);
+        return BLOB_LOOP_BREAK;
     }
 
     ctx->data_ptr += to_copy;
-    ctx->blob->header.effective_data_size = to_copy;
+    blob->header.effective_data_size = to_copy;
 
     if (ctx->data_ptr == ctx->data_len) {
-      ctx->rv = 0;
-      return BLOB_LOOP_BREAK;
+        submit_blob(blob);
+        ctx->return_value = 0;
+        return BLOB_LOOP_BREAK;
     }
 
-    // MAX_BLOBS allocated.
-    if (i == MAX_BLOBS - 1) {
-      submit_blob(ctx->blob);
-      ctx->blob = 0;
-      return BLOB_LOOP_BREAK;
-    }
+    ctx->blob_id = i < MAX_BLOBS - 1 ? next_blob_id() : 0;
+    blob->header.blob_next = ctx->blob_id;
 
-    ctx->blob = next_blob(ctx->blob);
+    submit_blob(blob);
     return BLOB_LOOP_CONTINUE;
 }
 
 // `copy_data_to_blob` copies data to blobs. This function returns
 // * 0 if it has succeeded;
-// * -1 if it has failed;
+// * -1 if not all data are copied;
 //
-// `blob_id` is the first blob submitted, even if the function has failed.
-// If no blobs are submitted, `blob_id` is 0.
+// `blob_id` is the first blob submitted or attempted to submit, even if the function has failed.
 // `data_len` is the length of the data to be copied.
 //
 // Maximum blobs supported by this function is MAX_BLOBS.
@@ -151,24 +172,15 @@ static s32 copy_data_to_blob(const void *src, const u64 data_len, u64 *blob_id, 
   blob_loop_context ctx  = {
       .src = (void *)src,
       .data_len = data_len,
-      .blob_id = blob_id,
+      .blob_id = next_blob_id(),
       .is_kernel = is_kernel,
-      .rv = -1,
+      .return_value = -1,
       .data_ptr = 0,
-      .blob = reserve_blob(),
   };
 
   bpf_loop(MAX_BLOBS, blob_loop_func, &ctx, 0);
 
-  if (ctx.blob) {
-    if (ctx.rv == 0) {
-      submit_blob(ctx.blob);
-    } else {
-      discard_blob(ctx.blob);
-    }
-  }
-
-  return ctx.rv;
+  return ctx.return_value;
 }
 
 #endif
